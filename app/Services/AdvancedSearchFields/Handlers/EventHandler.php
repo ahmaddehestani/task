e8a1b4a5390e43971176d455fdb3c2cb4059a517
ahmaddehestani <s.ahmad.dehestani@gmail.com>
2025-07-28 10:09:47 +0330

    resource

--- app/Services/AdvancedSearchFields/Handlers/EventHandler.php
+++ app/Services/AdvancedSearchFields/Handlers/EventHandler.php
@@ -4,10 +4,7 @@ declare(strict_types=1);
 
 namespace App\Services\AdvancedSearchFields\Handlers;
 
-use App\Enums\CargoWagonCountryTypeEnum;
-use App\Enums\CargoWagonTypeEnum;
-use App\Enums\TableUserFieldAdminConfirmedEnum;
-use App\Enums\WagonOwnershipEnum;
+
 
 class EventHandler extends BaseHandler
 {
